2d978c909e6ac43ce79d3b746e83216239ae0e5e
Tobi Denagan <denagantobi@gmail.com>
2025-09-16 22:54:32 +0100

    Ordering.Domain set up using clean architecture with domain driven design

---
+++ Ordering.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureServices
+        (this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("Database");
+
+        //services.AddDbContext<ApplicationDbContext>(OptionsBuilderConfigurationExtensions 
+        //    => OptionsBuilderConfigurationExtensions.UseSqlServer(connectionString));
+
+        //services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+
+        return services;
+    }
+}
---
+++ src/Services/Ordering/Ordering.Domain/Events/OrcerCreatedEvent.cs
@@ -0,0 +1,3 @@
+﻿namespace Ordering.Domain.Events;
+
+public record OrcerCreatedEvent(Order order) : IDomainEvent;
